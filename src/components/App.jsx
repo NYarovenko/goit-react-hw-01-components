61fa8cad1cc4d1a3b1218d8a9591b350afe1b103
NYarovenko <NYarovenko.Full.Stack@gmail.com>
2023-10-02 00:50:25 +0300

    _

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,8 +2,8 @@ import { GlobalStyle } from 'GlobalStyle';
 import user from '../data/user.json';
 import data from '../data/data.json';
 
-import { Profile } from './Profile/Profile';
 import { Statistics } from './Statistics/Statistics';
+import { Profile } from './Profile/Profile';
 
 export const App = () => {
   return (
